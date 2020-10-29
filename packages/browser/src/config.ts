90e75f6d21a8b268a24ceaed1e4b00359181af35
Curtis Liu <curtis@amplitude.com>
2020-10-29 09:21:46 -0700

    Alphabetize config options

--- packages/browser/src/config.ts
+++ packages/browser/src/config.ts
@@ -5,40 +5,46 @@
 
 export type SkylabConfig = {
   /**
-   * The instance name for the SkylabClient. Instance names are case _insensitive_.
+   * Set to true to log some extra information to the console.
    */
-  instanceName?: string;
-  /**
-   * The server endpoint from which to request variants.
-   */
-  serverUrl?: string;
+  debug?: boolean;
+
   /**
    * The default fallback variant for all {@link SkylabClient.getVariant} calls.
    */
   fallbackVariant?: string;
+
   /**
    * Initial variant values for flags. This is useful for bootstrapping the client with
    * values determined on the server.
    */
   initialFlags?: { [flagKey: string]: string };
+
   /**
-   * Whether to prioritize initialFlags over localStorage while async requests for variants are still in flight.
+   * The instance name for the SkylabClient. Instance names are case _insensitive_.
    */
-  preferInitialFlags: boolean;
+  instanceName?: string;
+
   /**
    * True if this client is being initialized on the server side. This is useful for server side rendering.
    * Currently this flag is unused but is reserved for future use.
    */
   isServerSide?: boolean;
+
   /**
-   * Set to true to log some extra information to the console.
+   * Whether to prioritize initialFlags over localStorage while async requests for variants are still in flight.
    */
-  debug?: boolean;
+  preferInitialFlags: boolean;
+
+  /**
+   * The server endpoint from which to request variants.
+   */
+  serverUrl?: string;
 };
 
 export const Defaults = {
-  INSTANCE_NAME: '$default_instance',
   FALLBACK_VARIANT: '',
-  SERVER_URL: 'https://api.lab.amplitude.com',
+  INSTANCE_NAME: '$default_instance',
   METADATA_STORAGE_KEY: 'amp-sl-meta',
+  SERVER_URL: 'https://api.lab.amplitude.com',
 };
